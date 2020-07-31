30708d64cf5b8728b0988520a1022f9632e073b3
Kirija Amalan <kirija.amalan@gmail.com>
2020-07-31 18:47:53 +1000

    testing blogpost Singapore-4

--- bp.js
+++ bp.js
@@ -4,9 +4,6 @@ let backgroundImg = document.querySelector("#backgroundImg");
 
 btnName.onclick = function(){
     alert("here5");
-    //backgroundImg.src = "none";
-    alert("here6");
     //document.getElementById("backgroundImg").style.backgroundColor = "blue";
-    //document.getElementById("backgroundImg").style.color = "blue";
-    backgroundImg.style.backgroundColor  = "orange";
+    document.body.style.backgroundColor = "orange";
 }
